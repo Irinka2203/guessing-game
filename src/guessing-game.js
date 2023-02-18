085bbd1663bbe5f67c52ce87e3b0625c9fb39860
Irinka2203 <irinkah2203@gmail.com>
2023-02-18 13:33:28 +0200

    feat: add a solution of the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.maxVal = max;
+    this.minVal = min;
     }
 
     guess() {
-
+        return Math.round((this.maxVal + this.minVal) / 2)
     }
 
     lower() {
-
+        return this.maxVal = (Math.round((this.maxVal + this.minVal) / 2));
     }
 
     greater() {
-
+        return  this.minVal = (Math.round((this.maxVal + this.minVal) / 2));
     }
 }
+
+
+
 
 module.exports = GuessingGame;
